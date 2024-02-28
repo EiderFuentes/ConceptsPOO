52c74075b751b226a9c8ee90df4a813921d749ea
Eider Fuentes <ing.eiderfuentes1989@gmail.com>
2024-02-28 17:31:31 -0500

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Date.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConceptsPOO
+{
+    public class Date
+    {
+        private int _year;
+        private int _month;
+        private int _day;
+
+        //Contructor
+        // ctor es el sniper para construir constructor con doble tab
+        public Date(int year, int month, int day)
+        {
+            _year = year;
+            _month = CheckMonth(month);
+            _day = CheckDay(year, month, day);
+        }
+
+        // Metodo del dia
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && IsleapYear(year))
+            {
+                return day;
+            }
+
+            // Arreglo 
+            int[] daysPerMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if (day >= 1 && day <= daysPerMonth[month] )
+            {
+                return day;
+            }
+
+            throw new DayException("Invalid day");
+        }
+
+        //Metodo año bisiesto
+        private bool IsleapYear(int year)
+        {
+
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+
+            //if (year % 4 == 0)
+            //{
+            //    if (year % 100 == 0 )
+            //    {
+
+            //        if (year % 400 == 0)
+            //        {
+
+            //            return true;
+            //        }
+            //        else
+            //        { 
+            //          return false;
+            //        }
+
+            //    }
+            //    else
+            //    { 
+            //        return true;
+            //    }
+
+            //}
+            //else
+            //{ 
+            //    return false;
+            //}
+             
+            throw new NotImplementedException();
+        }
+
+        //metodo del mes
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+
+            throw new MonthException("Invalid Month");
+        }
+
+        // sobreescribir  el ToString()
+        public override string ToString()
+        {
+            // interpobacion de ToString()
+            return $"{_year}/{_month:00}/{_day:00}";
+        }
+
+    }
+}
---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using ConceptsPOO;
+
+Console.WriteLine("Concepts POO");
+Console.WriteLine("==============");
+Date date1 = new Date(2022, 2, 11);
+
+//Console.WriteLine(date1);
+
+// atajos 
+// Ctrl + K + S saca el try y catch
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 30));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
